981461bba4a89796c24006d2a260b6b8b14396a1
Sergey Reshetnyak <sreshetniak@mirantis.com>
2017-11-29 17:23:16 +0300

    Add destroy SG action for helm provider

--- app/scripts/modules/helm/serverGroup/details/details.controller.js
+++ app/scripts/modules/helm/serverGroup/details/details.controller.js
@@ -4,16 +4,24 @@ const angular = require('angular');
 import _ from 'lodash';
 
 import {
+  CONFIRMATION_MODAL_SERVICE,
   SERVER_GROUP_READER,
+  SERVER_GROUP_WARNING_MESSAGE_SERVICE,
+  SERVER_GROUP_WRITER,
 } from '@spinnaker/core';
 
 module.exports = angular.module('spinnaker.serverGroup.details.helm.controller', [
   require('@uirouter/angularjs').default,
   require('../configure/configure.helm.module.js'),
   SERVER_GROUP_READER,
+  SERVER_GROUP_WRITER,
+  require('../paramsMixin.js'),
+  SERVER_GROUP_WARNING_MESSAGE_SERVICE,
+  CONFIRMATION_MODAL_SERVICE,
 ])
   .controller('helmServerGroupDetailsController', function ($scope, $state, app, serverGroup,
-                                                            serverGroupReader) {
+                                                            serverGroupReader, serverGroupWriter, helmServerGroupParamsMixin,
+                                                            serverGroupWarningMessageService, confirmationModalService) {
     let application = this.application = app;
 
     $scope.state = {
@@ -57,5 +65,44 @@ module.exports = angular.module('spinnaker.serverGroup.details.helm.controller',
         app.serverGroups.onRefresh($scope, retrieveServerGroup);
       }
     });
+
+    this.destroyServerGroup = () => {
+      var serverGroup = $scope.serverGroup;
+
+      var taskMonitor = {
+        application: application,
+        title: 'Destroying ' + serverGroup.name,
+      };
+
+      var submitMethod = (params) => serverGroupWriter.destroyServerGroup(
+        serverGroup,
+        application,
+        angular.extend(params, helmServerGroupParamsMixin.destroyServerGroup(serverGroup, application))
+      );
+
+      var stateParams = {
+        name: serverGroup.name,
+        accountId: serverGroup.account,
+        namespace: serverGroup.namespace
+      };
+
+      var confirmationModalParams = {
+        header: 'Really destroy ' + serverGroup.name + '?',
+        buttonText: 'Destroy ' + serverGroup.name,
+        account: serverGroup.account,
+        taskMonitorConfig: taskMonitor,
+        submitMethod: submitMethod,
+        askForReason: true,
+        onTaskComplete: () => {
+          if ($state.includes('**.serverGroup', stateParams)) {
+            $state.go('^');
+          }
+        },
+      };
+
+      serverGroupWarningMessageService.addDestroyWarningMessage(app, serverGroup, confirmationModalParams);
+
+      confirmationModalService.confirm(confirmationModalParams);
+    };
 
   });
